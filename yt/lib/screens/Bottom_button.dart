ce21756deab8e7fac182421b1db2e7cb92328348
Sarunwit00 <a.sarunwit@gmail.com>
2025-02-22 18:40:31 +0700

    save

--- yt/lib/screens/Bottom_button.dart
+++ yt/lib/screens/Bottom_button.dart
@@ -9,12 +9,21 @@ class BottomButton extends StatefulWidget {
 
 class _BottomButtonState extends State<BottomButton> {
   int _selectedIndex = 0;
+  //  final List<Widget> _screens = [
+  //   HomePage(),       
+  //   ShortsPage(),      
+  //   Container(),       
+  //   SubscriptionsPage(),
+  //   AccountPage(),
+  // ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: (i) => setState(() => _selectedIndex = i),
+        selectedFontSize: 10.0,
+        unselectedFontSize: 10.0,
         type: BottomNavigationBarType.fixed,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -25,21 +34,23 @@ class _BottomButtonState extends State<BottomButton> {
           BottomNavigationBarItem(
             icon: Icon(Icons.explore_outlined),
             activeIcon: Icon(Icons.explore),
-            label: 'Explore',
+            label: 'Shorts',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
+            icon: Icon(Icons.add_circle,size: 45,color: Colors.white),
+            activeIcon: Icon(Icons.add_circle),
             label: '',
           ),
+
           BottomNavigationBarItem(
             icon: Icon(Icons.subscriptions_outlined),
             activeIcon: Icon(Icons.subscriptions),
             label: 'Subscriptions',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.video_library_outlined),
-            activeIcon: Icon(Icons.video_library),
-            label: 'Library',
+            icon: Icon(Icons.account_circle_outlined),
+            activeIcon: Icon(Icons.account_circle),
+            label: 'Account',
           ),
         ],
         selectedItemColor: Colors.white,
